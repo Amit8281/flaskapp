b0d2ad2c42f641d30bdaa9d67286ec58dbdc70ad
Amit8281 <ak15072419@gmail.com>
2023-05-04 23:50:19 +0530

    flask

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/")#Decorator
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "<h1>Hello, World1!</h1>"
+
+@app.route("/hello_world1")#Decorator
+def hello_world1():
+    return "<h1>Hello, World2!</h1>"
+
+@app.route("/hello_world2")#Decorator
+def hello_world2():
+    return "<h1>Hello, World3!</h1>"
+
+@app.route("/test")
+def test():
+    return "this is my function to run app"
+    
+@app.route("/test2")
+def test2():
+    data= request.args.get('x')
+    return "this is a data input from my url {}".format(data)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
